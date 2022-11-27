07492e514c61907e8514643938f31e5d2d63bdc8
kolya <105161410+surfacewaves@users.noreply.github.com>
2022-11-27 23:28:25 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,19 @@
 function changeImage(anything) {
     document.getElementById('slider').src = anything;
 }
-    function toggleMenu() {
+function toggleMenu() {
     const menuToggle = document.querySelector('.menuToggle');
     const navigation = document.querySelector('.navigation');
     const line = document.querySelector('.line');
     menuToggle.classList.toggle('active');
     navigation.classList.toggle('active');
     line.classList.toggle('active');
+}
+function toggleAlbum(n) {
+    const button = document.querySelector('.button' + n);
+    const window = document.querySelector('.window' + n);
+    const shadow = document.querySelector('.shadow');
+    button.classList.toggle('active');
+    window.classList.toggle('active');
+    shadow.classList.toggle('active');
 }
